1a72960a3aa491467738dd87fba4119957fdcf90
Meliton Pal <meliton.pal@xenial.com>
2026-09-18 11:31:45 +0200

    Restore allapps

--- src/com/android/launcher3/touch/AbstractStateChangeTouchController.java
+++ src/com/android/launcher3/touch/AbstractStateChangeTouchController.java
@@ -90,7 +90,7 @@ public abstract class AbstractStateChangeTouchController
 
     @Override
     public boolean onControllerInterceptTouchEvent(MotionEvent ev) {
-    /*
+
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {
             mNoIntercept = !canInterceptTouch(ev);
             if (mNoIntercept) {
@@ -125,8 +125,7 @@ public abstract class AbstractStateChangeTouchController
 
         onControllerTouchEvent(ev);
         return mDetector.isDraggingOrSettling();
-        */
-        return false;
+
     }
 
     private int getSwipeDirection() {
